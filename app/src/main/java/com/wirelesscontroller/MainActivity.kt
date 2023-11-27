10687cc68cf63d253bfaf6ee9ad6201c91e33571
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-26 22:47:08 -0600

    Carga inicial de archivos

--- app/src/main/java/com/wirelesscontroller/MainActivity.kt
+++ app/src/main/java/com/wirelesscontroller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.wirelesscontroller
 
+import android.Manifest
 import android.os.Bundle
 import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
@@ -12,6 +13,11 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.wirelesscontroller.databinding.ActivityMainBinding
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,6 +45,37 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        // Configuración de Bluetooth
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+
+        if (bluetoothAdapter == null) {
+            // El dispositivo no admite Bluetooth
+        } else {
+            if (!bluetoothAdapter.isEnabled) {
+                // Bluetooth está apagado, solicitar al usuario que lo encienda
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivity(enableBtIntent)
+                //startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            } else {
+                // Bluetooth está activado, puedes comenzar a trabajar con él
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_SCAN
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                bluetoothAdapter.startDiscovery()
+                // Lógica para escanear o conectar dispositivos, etc.
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
